312ab1dec08db9567099956367074888a7d90311
Joseph Maa <josephgmaa@berkeley.edu>
2022-11-07 16:48:32 -0800

    Update nan dataframe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyspark
+from pyspark.sql.functions import col, isnan, when, count
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -8,13 +9,21 @@ DATASET_PATH = "US_Accidents_Dec21_updated.csv"
 OUTPUT_PATH = "outputs"
 
 
-def main():
-    spark = pyspark.sql.SparkSession.builder.getOrCreate()
-    accidents_df = spark.read.csv(DATASET_PATH, inferSchema=True, header=True)
+def generate_missing_values(accidents_df):
+    missing_df = [
+        count(
+            when(
+                col(c).contains("None") | col(c).contains("NULL") | col(c)
+                == " " | col(c).isNull() | isnan(c),
+                c,
+            )
+        ).alias(c)
+        for c in accidents_df.columns
+    ]
+    missing_df.show()
 
-    # Show the dataframe schema.
-    accidents_df.printSchema()
 
+def generate_histogram(accidents_df):
     # Plot the count of accidents by state.
     states = accidents_df.select("State").distinct().collect()
     counts = []
@@ -29,7 +38,17 @@ def main():
     plt.show()
     plt.savefig(os.path.join("outputs", "histogram.png"))
 
-    
+
+def main():
+    spark = pyspark.sql.SparkSession.builder.getOrCreate()
+    accidents_df = spark.read.csv(DATASET_PATH, inferSchema=True, header=True)
+
+    # Show the dataframe schema.
+    accidents_df.printSchema()
+
+    # generate_histogram(accidents_df)
+    generate_missing_values(accidents_df)
+
 
 if __name__ == "__main__":
     main()
